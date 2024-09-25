e186aa7ffa76de0cc6c8207e49607e805089dfed
Nathan t <tsai.nathan.2009@gmail.com>
2024-09-24 21:43:21 -0700

    I don't want to break stubborn pins

--- 15.ino
+++ 15.ino
@@ -2,25 +2,41 @@
 // br my wifi isn't cooperating with me :(
 // can't use a motor whyyyyyyyyyyyy?
 // what are l293d boards??
+// compare to .example.ino
+// it worked
+// after realizing that the L293D board was not pushed in enough
+// its sooo annoying to push button/ hold motor down 'cause half the time it doesn't work
 const int in1 = 10;
 const int in2 = 9;
-const int mot = 12;
+const int mot = 12;\\\
 
 void setup()
 {
     pinMode(in1, OUTPUT);
     pinMode(in2, OUTPUT);
-    pinMode(mot, OUTPUT);
+    pinMode(mot, OUTPUT);\\\
 }
 
 void loop()
-{
+{ 
     digitalWrite(in1, LOW);
-    digitalWrite(in2, LOW);
+    digitalWrite(in2, LOW);\\\
+    // use analog, not digital for right applications
     digitalWrite(mot, LOW);
     delay(1000)
     digitalWrite(in1, HIGH);
-    digitalWrite(in2, HIGH);
-    digitalWrite(mot, HIGH);
+    digitalWrite(in2, LOW);
+    digitalWrite(mot, HIGH);\\\
+    delay(1000)
+
+
+    /*************************************************/
+    // clockwise
+    analogWrite(in1, 200);
+    analogWrite(in2, 0);
+    delay(1000);
+    // or not
+    analogWrite(in2, 0);
+    analogWrite(in2, 200);
     delay(1000)
 }
---
+++ l6.ino
@@ -0,0 +1,20 @@
+// let's see... 
+// btw im using tutorial super kit v2.0 sunfounder
+// also im not going to do flowing lights bc its too easy
+// and I would like to finish the arduino:   (~~)
+//                                          \( *)/
+//                                            /\
+//WHAT IS LCD1602???
+// ig theres lots to learn?
+// ohh its not that bad :)
+// aand a lot of wires!too many
+// im doing command line arguments
+void start()
+{
+    // eerm
+}
+
+void setup()
+{
+     
+}
